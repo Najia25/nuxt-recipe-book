8a7d45b0a6ec2230146b5d7ea174411f6b961d53
Najia Afrin <najiaafrin2@gmail.com>
2020-02-26 18:30:49 +0600

    initial setup

---
+++ pages/recipes/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="recipes">
+    <article class="recipe">
+      <div></div>
+      <h1>Title</h1>
+      <p>SOme nice text</p>
+    </article>
+    <article class="recipe">
+      <div></div>
+      <h1>Title</h1>
+      <p>SOme nice text</p>
+    </article>
+  </section>
+</template>
+
+<style scoped>
+
+.recipes{
+  display:flex;
+  flex-wrap: wrap;
+}
+
+.recipe {
+  box-sizing:border-box;
+  width:280px;
+  padding:8px;
+  border:1px solid #ccc;
+  box-shadow: 0 2px 2px #aaa
+}
+</style>
